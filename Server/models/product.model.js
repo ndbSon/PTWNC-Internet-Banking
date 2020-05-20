2d6e6857f179841dae44b0eb95f5491f23f3b35c
ndbSon <49903834+ndbSon@users.noreply.github.com>
2020-05-20 22:02:21 +0700

    delete

--- Server/models/product.model.js
+++
@@ -1,6 +0,0 @@
-const db = require('../utils/db');
-
-module.exports = {
-  all: _ => db.load('select * from products'),
-  detail: id => db.load(`select * from products where ProID = ${id}`),
-};
--- Server/models/user.model.js
+++
@@ -1,6 +0,0 @@
-const db = require('../utils/db');
-
-module.exports = {
-  all: _ => db.load('select * from user'),
-  detail: id => db.load(`select * from user where UserID = ${id}`),
-};
