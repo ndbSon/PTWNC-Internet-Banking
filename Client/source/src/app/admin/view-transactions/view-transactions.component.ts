5b08cfa15a08a67ca079610829bc4d232ad8ca03
TanCao98 <heocaominhtan@gmail.com>
2020-07-04 13:21:53 +0700

    done transaction

--- Client/source/src/app/admin/view-transactions/view-transactions.component.ts
+++ Client/source/src/app/admin/view-transactions/view-transactions.component.ts
@@ -3,6 +3,8 @@ import { AdminService } from "src/app/services/admin.service";
 import { Router } from "@angular/router";
 import { DatatableComponent } from "@swimlane/ngx-datatable";
 import { ToastrService } from "ngx-toastr";
+import * as moment from 'moment';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: "app-view-transactions",
@@ -14,19 +16,36 @@ export class ViewTransactionsComponent implements OnInit {
   myFilterTable: DatatableComponent;
   debitRows: any[];
   tranRows: any[];
+  selected1: any;
+  total1: number;
+  body: {};
   constructor(
     private service: AdminService,
     private router: Router,
     private ms: ToastrService
   ) {}
-  begin: string;
-  end: string;
+  begin = "2020-01-20";
+  end = "2020-10-20";
   ngOnInit() {
+    this.submit();
+  }
+  ngModelChange(e){
+    if (e.start){
+      this.begin = e.start.format('YYYY-MM-DD');
+    }
+    if (e.end){
+      this.end = e.end.format('YYYY-MM-DD');
+    }
+    this.submit();
+  }
+  
+  submit(){
+    this.body = { begin: this.begin, end: this.end };
     this.service
-      .getListTransaction({ begin: this.begin, end: this.end })
+      .getListTransaction(this.body)
       .subscribe((res: any) => {
         this.debitRows = res.debit;
-        this.tranRows= res.trans;
+        this.tranRows = res.trans;
       });
   }
 }
--- Client/source/src/app/services/admin.service.ts
+++ Client/source/src/app/services/admin.service.ts
@@ -1,34 +1,37 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { environment } from "src/environments/environment";
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 export class AdminService {
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-    url: string = environment.apiUrlBankRsa + "/admin";
+  url: string = environment.apiUrlBankRsa + "/admin";
 
-    postSignup(body) {
-        return this.http.post<any>(`${this.url}/signup`, body);
-    }
-    postEdit(body) {
-        return this.http.post<any>(`${this.url}/update`, body);
-    }
-    postLock(id) {
-        return this.http.post<any>(`${this.url}/lockacount`, {"Id": id});
-    }
-    postUnlock(id) {
-        return this.http.post<any>(`${this.url}/openlock`, {"Id": id});
-    }
-    getListTransaction(body) {
-        return this.http.get<any>(`${this.url}/transaction`, body);
-    }
-    getTransbank(body) {
-        return this.http.post<any>(`${this.url}/transbank`, body);
-    }
-    getListAccount() {
-        return this.http.get<any>(`${this.url}/account`);
-    }
-    getAccount(body) {
-        return this.http.get<any>(`${this.url}/detail`, body);
-    }
+  postSignup(body) {
+    return this.http.post<any>(`${this.url}/signup`, body);
+  }
+  postEdit(body) {
+    return this.http.post<any>(`${this.url}/update`, body);
+  }
+  postLock(id) {
+    return this.http.post<any>(`${this.url}/lockacount`, { Id: id });
+  }
+  postUnlock(id) {
+    return this.http.post<any>(`${this.url}/openlock`, { Id: id });
+  }
+  getListTransaction(body) {
+    const params = new HttpParams()
+      .set("begin", body.begin)
+      .set("end", body.end);
+    return this.http.get<any>(`${this.url}/transaction`, { params });
+  }
+  getTransbank(body) {
+    return this.http.post<any>(`${this.url}/transbank`, body);
+  }
+  getListAccount() {
+    return this.http.get<any>(`${this.url}/account`);
+  }
+  getAccount(body) {
+    return this.http.get<any>(`${this.url}/detail`, body);
+  }
 }
